6b5eabb946348192da135fdddf9c6257c0f840bf
Khiem Le <theinnocentman1@gmail.com>
2021-01-09 20:13:12 +0700

    Support OpenGraph

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ module.exports = {
 	chainWebpack: (config) => {
 		config.plugin("html").tap((args) => {
 			args[0].title = "UIT Schedule";
+			args[0].description =
+				"Công cụ cho sinh viên UIT, dễ dàng sắp xếp TKB của mình!";
+			args[0].url = "https://khiemledev.github.io/uit-schedule";
+			args[0].domain = "khiemledev.github.io";
 			return args;
 		});
 	}
