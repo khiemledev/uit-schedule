c13e2a79ff02e74a159bc318a19c01084fb995c9
Khiem Le <theinnocentman1@gmail.com>
2021-01-09 18:47:44 +0700

    Change App title and favicon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/uit-schedule/" : "/"
+	publicPath: process.env.NODE_ENV === "production" ? "/uit-schedule/" : "/",
+	chainWebpack: (config) => {
+		config.plugin("html").tap((args) => {
+			args[0].title = "UIT Schedule";
+			return args;
+		});
+	}
 };
