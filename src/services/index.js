09af369bac313095ef3f5ea1dcc55d5d7e774879
khiemledev <theinnocentman1@gmail.com>
2021-01-18 09:44:39 +0700

    Add check expire days

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,43 @@ const isStorageSupported = typeof Storage !== "undefined";
 const CLASSES = "classes";
 const SELECTED_CLASSES = "selectedClasses";
 const SELECTED_CLASSES_DETAIL = "selectedClassesDetail";
+const STORE_TIME = "storeTime";
+const EXPIRE_DAYS = 30;
+
+function getDiffDate(a, b) {
+	let diffTime = Math.abs(a.getTime() - b.getTime());
+	return Math.round(diffTime / (24 * 3600 * 1000));
+}
+
+function setStoreTime() {
+	if (isStorageSupported) {
+		localStorage.setItem(STORE_TIME, Date.now());
+	}
+}
+
+function checkIfExpired() {
+	if (isStorageSupported) {
+		let time = localStorage.getItem(STORE_TIME);
+		if (!time) {
+			localStorage.clear();
+			return true;
+		}
+		let currentTime = new Date();
+		let storedTime = new Date(parseInt(time, 10));
+		let diffDate = getDiffDate(storedTime, currentTime);
+		if (diffDate > EXPIRE_DAYS) {
+			localStorage.clear();
+			return true;
+		}
+		return false;
+	}
+	return false;
+}
 
 function storeClasses(classes) {
 	if (isStorageSupported) {
 		localStorage.setItem(CLASSES, JSON.stringify(classes));
+		setStoreTime();
 	}
 }
 
@@ -27,9 +60,14 @@ function storeSelectedClassesDetail(classesDetail) {
 function retrieveClasses() {
 	let classes = null;
 	if (isStorageSupported) {
-		let data = localStorage.getItem(CLASSES);
-		if (data) {
-			classes = JSON.parse(data);
+		if (!checkIfExpired()) {
+			let data = localStorage.getItem(CLASSES);
+			if (data) {
+				classes = JSON.parse(data);
+			} else {
+				classes = require("../assets/TKB.json");
+				storeClasses(classes);
+			}
 		} else {
 			classes = require("../assets/TKB.json");
 			storeClasses(classes);
